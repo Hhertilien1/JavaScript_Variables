7852d86e2d828c6b7a23547578513bf2b7177c65
Hhertilien1 <kinghertilien@gmail.com>
2024-01-16 22:42:40 -0500

    Done

--- app.js
+++ app.js
@@ -4,7 +4,46 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
 
 // Exercise 2
+const firstName = Herby;
+
+lastName = Hertilien;
+
+age = 25;
 
 // Exercise 3
+
+let language = "JavaScript";
+
+let createdYear = 1995;
+
+let isCaseSensitive = true;
+
+// exercise 4
+
+let price = 19.99;
+
+let isOnSale = false;
+
+let salesPercentage = 15;
+
+let stock = 0;
+
+let inStock = false;
+
+let selectedSize = "M";
+
+// Exercise 5
+
+let title = "Name of the Wind";
+
+let author = "Patrick Rothfuss";
+
+let pageCount = 722;
+
+let bookmark = 456; 
+
+let hasRead = true;
